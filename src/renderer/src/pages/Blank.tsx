9e7adb29d69f2946716fd7a3cc2c24ea84522fca
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-10-23 16:08:07 -0300

    feat: add side bar in  black

--- src/renderer/src/pages/Blank.tsx
+++ src/renderer/src/pages/Blank.tsx
@@ -1,10 +1,19 @@
-import { Link } from 'react-router-dom'
-
+import { ToC } from '../components/ToC'
 export function Blank() {
   return (
-    <main className="flex flex-1 items-center justify-center text-rotion-400">
-      Selecione ou crie um documento
-      <Link to="/document">Acessar Documento</Link>
+    <main className="flex flex-1  py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-rotion-300 font-semibold text-xs">
+          TABLE OF CONTENT
+        </span>
+        <ToC.Root>
+          <ToC.Section>
+            <ToC.Link>BackEnd</ToC.Link>
+            <ToC.Link>Banco de Dados</ToC.Link>
+          </ToC.Section>
+        </ToC.Root>
+      </aside>
+      <section className="flex-1 flex flex-col items-center">fasdfasd</section>
     </main>
   )
 }
