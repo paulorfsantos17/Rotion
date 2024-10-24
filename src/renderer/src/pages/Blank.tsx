dff9193812a70475c2e77727c7308e0e3df0ca3f
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-10-24 16:23:14 -0300

    feat: add content text in blackPage

--- src/renderer/src/pages/Blank.tsx
+++ src/renderer/src/pages/Blank.tsx
@@ -1,22 +1,9 @@
-import { Editor } from '../components/Editor'
-import { ToC } from '../components/ToC'
 export function Blank() {
   return (
-    <main className="flex flex-1  py-12 px-10 gap-8">
-      <aside className="hidden lg:block sticky top-0">
-        <span className="text-rotion-300 font-semibold text-xs">
-          TABLE OF CONTENT
-        </span>
-        <ToC.Root>
-          <ToC.Section>
-            <ToC.Link>BackEnd</ToC.Link>
-            <ToC.Link>Banco de Dados</ToC.Link>
-          </ToC.Section>
-        </ToC.Root>
-      </aside>
-      <section className="flex-1 flex flex-col items-center">
-        <Editor />
-      </section>
+    <main className="flex flex-1  items-center justify-center py-12 px-10 gap-8">
+      <span className="text-rotion-300 text-xl">
+        Selecione ou crie um documento
+      </span>
     </main>
   )
 }
