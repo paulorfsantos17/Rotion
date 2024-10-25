e6b61abf4a1cebf21ba4fc42f92a54009dfa83dd
Paulo Ricardo Santos <42043880+paulorfsantos17@users.noreply.github.com>
2024-10-25 14:49:02 -0300

    feat: add searchBar

--- src/renderer/src/components/SearchBar/index.tsx
+++ src/renderer/src/components/SearchBar/index.tsx
@@ -1,26 +1,43 @@
+import { useQuery } from '@tanstack/react-query'
 import { Command } from 'cmdk'
 import { File, MagnifyingGlass } from 'phosphor-react'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-export function SearchBar() {
-  const [open, setOpen] = useState(false)
+interface SearchBarProps {
+  open: boolean
+  onOpenChange: () => void
+}
+
+export function SearchBar({ onOpenChange, open }: SearchBarProps) {
+  const navigate = useNavigate()
+  const { data: response } = useQuery({
+    queryKey: ['documents'],
+    queryFn: window.api.fetchDocuments,
+    refetchOnWindowFocus: true,
+  })
+
+  function handleOpenDocument(id: string) {
+    navigate(`/document/${id}`)
+    onOpenChange()
+  }
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
-      if (e.key === 'k' && e.metaKey) {
-        setOpen((state) => !state)
+      if (e.key === 'k') {
+        onOpenChange()
       }
     }
 
     document.addEventListener('keydown', down)
     return () => document.removeEventListener('keydown', down)
-  }, [setOpen])
+  }, [onOpenChange, open])
 
   return (
     <Command.Dialog
       className="fixed top-24 left-1/2 -translate-x-1/2 w-[480px] max-w-full bg-rotion-800 rounded-md shadow-2xl text-rotion-100 border border-rotion-600"
       open={open}
-      onOpenChange={setOpen}
+      onOpenChange={onOpenChange}
       label="Search"
     >
       <div className="flex items-center gap-2 border-b border-rotion-700 p-4">
@@ -31,30 +48,23 @@ export function SearchBar() {
           className="w-full bg-transparent focus:outline-none text-sm text-rotion-50 placeholder:text-rotion-200"
         />
       </div>
-      <Command.List className="py-2 max-h-48 scrollbar-thin scrollbar-thumb-rotion-600 scrollbar-track-rotion-800">
+      <Command.List className="py-2 max-h-48 scrollbar-thin scrollbar-thumb-rotion-600 scrollbar-track-rotion-800 overflow-y-scroll">
         <Command.Empty className="py-3 px-4 text-rotion-200 text-sm">
           Nenhum documento encontrado.
         </Command.Empty>
 
-        <Command.Item className="py-3 px-4 text-rotion-50 text-sm flex items-center gap-2 hover:bg-rotion-700 aria-selected:!bg-rotion-600">
-          <File className="w-4 h-4" />
-          Untitled
-        </Command.Item>
-
-        <Command.Item className="py-3 px-4 text-rotion-50 text-sm flex items-center gap-2 hover:bg-rotion-700 aria-selected:!bg-rotion-600">
-          <File className="w-4 h-4" />
-          Ignite
-        </Command.Item>
-
-        <Command.Item className="py-3 px-4 text-rotion-50 text-sm flex items-center gap-2 hover:bg-rotion-700 aria-selected:!bg-rotion-600">
-          <File className="w-4 h-4" />
-          Discover
-        </Command.Item>
-
-        <Command.Item className="py-3 px-4 text-rotion-50 text-sm flex items-center gap-2 hover:bg-rotion-700 aria-selected:!bg-rotion-600">
-          <File className="w-4 h-4" />
-          Rocketseat
-        </Command.Item>
+        {response?.data.map((document) => {
+          return (
+            <Command.Item
+              key={document.id}
+              onSelect={() => handleOpenDocument(document.id)}
+              className="py-3 px-4 text-rotion-50 text-sm flex items-center gap-2 hover:bg-rotion-700 aria-selected:!bg-rotion-600"
+            >
+              <File className="w-4 h-4" />
+              {document.title}
+            </Command.Item>
+          )
+        })}
       </Command.List>
     </Command.Dialog>
   )
